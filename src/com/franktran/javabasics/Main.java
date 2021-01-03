50d87b95f2f63935f124091cd5081c9b83b6ae8c
frank <franktranvantu@gmail.com>
2021-01-03 11:42:36 +0700

    Introduction to your tool

--- src/com/franktran/javabasics/Main.java
+++ src/com/franktran/javabasics/Main.java
@@ -1,11 +1,11 @@
 package com.franktran.javabasics;
 
-import java.io.Console;
+import java.io.PrintStream;
 
 public class Main {
 
     public static void main(String[] args) {
-        Console console = System.console();
-        console.printf("Hello, My name is Frank");
+        PrintStream ps = System.out;
+        ps.printf("Hello, My name is Frank");
     }
 }
