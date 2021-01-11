4f98d355768ef0d4a3cf0732b8470888527d7be4
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-11 10:06:16 +0700

    Update

--- src/com/franktran/javabasics/Introductions.java
+++ src/com/franktran/javabasics/Introductions.java
@@ -2,11 +2,11 @@ package com.franktran.javabasics;
 
 import java.io.Console;
 
-public class Main {
+public class Introductions {
 
     public static void main(String[] args) {
         Console console = System.console();
-        String name = "Frank";
+        String name = console.readLine("What is your name? ");
         console.printf("Hello, My name is %s\n", name);
         console.printf("%s is learning how to write Java\n", name);
     }
