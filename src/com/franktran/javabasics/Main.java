db6822b43e510b893c45c4885ee651fb1c3374ff
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-11 16:54:04 +0700

    Update

--- src/com/franktran/javabasics/Main.java
+++ src/com/franktran/javabasics/Main.java
@@ -15,7 +15,15 @@ public class Main {
             console.printf("Sorry, you must be at least 13 to use this program.\n");
             System.exit(0);
         }
-        String name = console.readLine("Enter your name: ");
-        console.printf("Your name is %s", name);
+        String name = console.readLine("Enter a name: ");
+        String adjective = console.readLine("Enter an adjective: ");
+        String noun = console.readLine("Enter a noun: ");
+        String adverb = console.readLine("Enter a adverb: ");
+        String verb = console.readLine("Enter a verb ending in -ing: ");
+
+        console.printf("Your summary:\n");
+        console.printf("---------------\n");
+        console.printf("%s is a %s %s. ", name, adjective, noun);
+        console.printf("The are always %s %s\n", adverb, verb);
     }
 }
