94098541def6d37584cab091e357fdbdb0819243
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-11 17:00:54 +0700

    Update

--- src/com/franktran/javabasics/Main.java
+++ src/com/franktran/javabasics/Main.java
@@ -18,6 +18,10 @@ public class Main {
         String name = console.readLine("Enter a name: ");
         String adjective = console.readLine("Enter an adjective: ");
         String noun = console.readLine("Enter a noun: ");
+        if (name.equalsIgnoreCase("dork")) {
+            console.printf("That language is not allowed. Exiting.");
+            System.exit(0);
+        }
         String adverb = console.readLine("Enter a adverb: ");
         String verb = console.readLine("Enter a verb ending in -ing: ");
 
