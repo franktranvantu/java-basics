9266419d96cc086bf17f66ca95bce0aaf7d7212e
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-11 12:22:17 +0700

    Update

---
+++ src/com/franktran/javabasics/ParsingInts.java
@@ -0,0 +1,14 @@
+package com.franktran.javabasics;
+
+import java.io.Console;
+
+/**
+ * @author frank.tran
+ */
+public class ParsingInts {
+
+    public static void main(String[] args) {
+        // Create a new int variable that is named answer. Store the integer version of answerToLife in it.
+        String answerToLife = "42";
+    }
+}
---
+++ src/com/franktran/javabasics/Solution.java
@@ -0,0 +1,13 @@
+package com.franktran.javabasics;
+
+/**
+ * @author frank.tran
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        // Create a new int variable that is named answer. Store the integer version of answerToLife in it.
+        String answerToLife = "42";
+        int answer = Integer.parseInt(answerToLife);
+    }
+}
